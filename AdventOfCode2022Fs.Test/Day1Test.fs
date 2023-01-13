7b97e5356d73f917418da5e23512b69ced752e7f
Arthur <arthrp@users.noreply.github.com>
2023-01-14 00:58:08 +0200

    Add unit tests

---
+++ AdventOfCode2022Fs.Test/Day1Test.fs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2022Fs.Test
+
+open NUnit.Framework
+
+[<TestFixture>]
+type Day1Tests () =
+
+    [<Test>]
+    member this.Day1SolvedCorrectly() =
+        let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n"
+        let result = AdventOfCode2022Fs.Day1.solveDay1 input
+        Assert.AreEqual(11000, result)
